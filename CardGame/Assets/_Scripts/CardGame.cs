fec0a239dda002744c1db8d2cc2de274986e8f9b
Mikhail <46933536+MesseMMP@users.noreply.github.com>
2024-03-04 17:10:02 +0300

    feat: create Unity objects for Layouts, update CardGame.cs
    Creating Layouts for Deck, Players, Discard and Center of Table. Adding method MoveToLayout to CardGame.cs and updating methods of Creating card to create them in Deck.

--- CardGame/Assets/_Scripts/CardGame.cs
+++ CardGame/Assets/_Scripts/CardGame.cs
@@ -30,13 +30,15 @@ public class CardGame : MonoBehaviour
     {
         foreach (CardAsset cardAsset in initialCards)
         {
-            CreateCard(cardAsset);
+            CreateCard(cardAsset, 1);
         }
     }
 
-    private void CreateCard(CardAsset cardAsset)
+    private void CreateCard(CardAsset cardAsset, int layoutId)
     {
         CardInstance cardInstance = new CardInstance(cardAsset);
+        int cardPosition = GetCardsInLayout(layoutId).Count + 1;
+        cardInstance.MoveToLayout(layoutId, cardPosition);
         CreateCardView(cardInstance);
     }
 
@@ -47,8 +49,15 @@ public class CardGame : MonoBehaviour
         cardView.Init(cardInstance);
 
         _cardInstances.Add(cardInstance, cardView);
+
+        MoveToLayout(cardInstance, cardInstance.LayoutId);
     }
-    
+
+    private void MoveToLayout(CardInstance cardInstance, int newLayoutId)
+    {
+        int cardPosition = GetCardsInLayout(newLayoutId).Count + 1;
+        cardInstance.MoveToLayout(newLayoutId, cardPosition);
+    }
     public List<CardView> GetCardsInLayout(int layoutId)
     {
         return (from pair in _cardInstances where pair.Key.LayoutId == layoutId select pair.Value).ToList();
